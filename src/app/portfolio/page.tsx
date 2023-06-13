d1a092e35923e1e756b3ba8c4bf90434ef3e8606
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-06-13 23:49:01 +0330

    ✨ feat:Implement portfolio component and styles.

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,22 @@
+import styles from "@/app/portfolio/page.module.css";
+import Link from "next/link";
+const Portfolio = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>choose a gallery</h1>
+      <div className={styles.items}>
+        <Link href="/portfolio/illustrations" className={styles.item}>
+          <span className={styles.title}>Illustrations</span>
+        </Link>
+        <Link href="/portfolio/websites" className={styles.item}>
+          <span className={styles.title}>Websites</span>
+        </Link>
+        <Link href="/portfolio/application" className={styles.item}>
+          <span className={styles.title}>Application</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
